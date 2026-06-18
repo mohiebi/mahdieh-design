aa2ac84a0121854e178fa9a63df6ec1ee31e56b2
Mohi <e.mohamadhosein@gmail.com>
2026-06-18 17:47:02 +0330

    updating config

--- config/services.php
+++ config/services.php
@@ -3,4 +3,18 @@
 return [
     'calendly_url' => env('CALENDLY_URL'),
     'contact_email' => env('CONTACT_EMAIL'),
+    'translation' => [
+        'provider' => env('TRANSLATION_PROVIDER', 'azure'),
+        'german_target_lang' => env('TRANSLATION_GERMAN_TARGET_LANG', 'de'),
+        'persian_target_lang' => env('TRANSLATION_PERSIAN_TARGET_LANG', 'fa'),
+    ],
+    'azure_translator' => [
+        'key' => env('AZURE_TRANSLATOR_KEY'),
+        'endpoint' => env('AZURE_TRANSLATOR_ENDPOINT', 'https://api.cognitive.microsofttranslator.com'),
+        'region' => env('AZURE_TRANSLATOR_REGION'),
+    ],
+    'deepl' => [
+        'auth_key' => env('DEEPL_AUTH_KEY'),
+        'api_base_url' => env('DEEPL_API_BASE_URL', 'https://api-free.deepl.com'),
+    ],
 ];
